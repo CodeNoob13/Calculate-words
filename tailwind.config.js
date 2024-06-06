6a9ac1867f5de1d68b690d738874f3c01607e79a
Glenn <glennbauchart@gmail.com>
2024-06-06 21:51:20 +0200

    Add Markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-black': '#30363d',
+        'custom-white': '#f6f8fa',
+        'custom-green': 'rgb(126, 231, 135)',
+        'call-to-action': '#dd7df7',
+      },
+    },
   },
   plugins: [],
-}
-
+};
